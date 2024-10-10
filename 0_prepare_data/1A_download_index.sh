d1838f5206be224f5b5d13daf428c9bc9da7c879
jessica-ewald <1jess.ewald@gmail.com>
2024-10-09 21:20:55 -0400

    refactor data prep files

--- 0_prepare_data/1A_download_index.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install the cpgdata package after installing Rust and Cargo
-python -m pip install cpgdata
-
-# Sync the cpg index to the specified directory
-cpg index sync -o "../1_snakemake/inputs/cpg_index"
