614a8749a3af261da0cd840a25a71318f6bb079c
Jess Ewald <jewald@ebi.ac.uk>
2025-02-07 15:42:26 -0500

    make metadata download more robust

--- 0_prepare_data/1A_download_metadata.py
+++ 0_prepare_data/1A_download_metadata.py
@@ -5,8 +5,10 @@ Download all Axiom platemap and biochem metadata.
 """  # noqa: CPY001, INP001
 
 import re
+import os
 
 from sh import aws
+from pathlib import Path
 
 
 def main() -> None:
@@ -19,6 +21,9 @@ def main() -> None:
     local_meta = "../1_snakemake/inputs/metadata"
     batches = ["prod_25", "prod_26", "prod_27", "prod_30"]
 
+    os.makedirs(f"{local_meta}/metadata", exist_ok=True)
+    os.makedirs(f"{local_meta}/biochem", exist_ok=True)
+
     # get metadata (both biochem.parquet and metadata.parquet)
     for batch in batches:
         batch_path = f"{aws_path}/{batch}/"
@@ -26,8 +31,15 @@ def main() -> None:
         plates = re.findall(r"plate_\d{8}", aws_output)
 
         for plate in plates:
-            aws("s3", "cp", f"{batch_path}{plate}/metadata.parquet", f"{local_meta}/metadata/metadata_{plate}.parquet")
-            aws("s3", "cp", f"{batch_path}{plate}/biochem.parquet", f"{local_meta}/biochem/biochem_{plate}.parquet")
+            metadata_file = Path(f"{local_meta}/metadata/metadata_{plate}.parquet")
+            biochem_file = Path(f"{local_meta}/biochem/biochem_{plate}.parquet")
+
+            # only download if the files doesn't already exist
+            if not metadata_file.exists():
+                aws("s3", "cp", f"{batch_path}{plate}/metadata.parquet", str(metadata_file))
+
+            if not biochem_file.exists():
+                aws("s3", "cp", f"{batch_path}{plate}/biochem.parquet", str(biochem_file))
 
 
 if __name__ == "__main__":
